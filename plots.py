ae39223e283d08620255fe28f363b12d98ca547b
maria-yankova <maria.yankova@postgrad.manchester.ac.uk>
2017-09-05 11:54:05 +0100

    Add plot_pole_figure todo

--- plots.py
+++ plots.py
@@ -3,6 +3,7 @@ import lattice
 import projections
 from matplotlib import pyplot as plt
 
+
 def plot_pole_fig(proj_poles, crys=None, pole=None, lattice_sys=None):
     """
     Return a figure object for a pole figure.
@@ -23,7 +24,7 @@ def plot_pole_fig(proj_poles, crys=None, pole=None, lattice_sys=None):
         Lattice system is one of cubic, hexagonal, rhombohedral, tetragonal, 
         orthorhombic, monoclinic, triclinic.
 
-    
+
     Returns
     -------
     f : matplotlib figure
@@ -33,7 +34,8 @@ def plot_pole_fig(proj_poles, crys=None, pole=None, lattice_sys=None):
     We can either plot multiple poles for a single crystal or 
 
     TODO:
-    - Label plot based on lattice system.
+    - Sort out plot labelling: based on lattice system for single crystal, and
+        based on plotted pole for polycrystal.
     - Add option for proj_poles to be ndarray.
 
     """
@@ -41,16 +43,15 @@ def plot_pole_fig(proj_poles, crys=None, pole=None, lattice_sys=None):
     all_crys = ['single', 'poly']
     if crys not in all_crys:
         raise ValueError('"{}" is not a valid crystal type. '
-                             '`crys` must be one of: {}.'.format(
-                                 crys, all_crys))
-    
-    if crys == 'poly' and  not pole:
-        raise ValueError('Please specify which pole is to be plotted using' 
-        'Miller(-Bravais) indices (for example \'001\').')
+                         '`crys` must be one of: {}.'.format(
+                             crys, all_crys))
 
+    if crys == 'poly' and not pole:
+        raise ValueError('Please specify which pole is to be plotted using'
+                         'Miller(-Bravais) indices (for example \'001\').')
 
-    f,ax = plt.subplots(1, 1, subplot_kw={'polar':True},figsize=(5,5))
-    ax.scatter(proj_poles[0],proj_poles[1])
+    f, ax = plt.subplots(1, 1, subplot_kw={'polar': True}, figsize=(5, 5))
+    ax.scatter(proj_poles[0], proj_poles[1])
 
     ax.set_title("Stereographic projection", va='bottom')
     ax.set_rmax(1)
@@ -60,7 +61,7 @@ def plot_pole_fig(proj_poles, crys=None, pole=None, lattice_sys=None):
     else:
         ax.set_xticklabels([])
         ax.annotate('[0001]', xy=(0, 0), xytext=(10.3, 0.15))
-    
+
     ax.set_yticklabels([])
-    
+
     return f
