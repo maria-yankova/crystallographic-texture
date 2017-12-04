7141e986c55d46674a684defe5f44150489105d9
APlowman <adam.jp@live.co.uk>
2017-12-04 22:09:44 +0000

    Add gb_disorientation

--- misorientations.py
+++ misorientations.py
@@ -1,4 +1,5 @@
 import coordgeometry
+import lattice
 import projections
 import numpy as np
 import rotations
@@ -87,11 +88,6 @@ def euler_pair_disorientation(eulers_A, eulers_B, point_group,
     Calculate disorientation axis and angle between a pair of orientations
     given in Euler angles in Bunge notation. Direction for rotation is A to B.
 
-    Notes
-    -----
-    Euler angles are expected in Bunge notation (intrinsic, ZXZ passive 
-    rotations, i.e. from sample to crystal coordinate system).
-
     Parameters
     ----------
     eulers_A : ndarray of shape (3,)
@@ -113,12 +109,18 @@ def euler_pair_disorientation(eulers_A, eulers_B, point_group,
         axis : ndarray of shape (3, 1)
         angle : float
             Disorientation angle in radians or degrees.
+
+    Notes
+    -----
+    Euler angles are expected in Bunge notation (intrinsic, ZXZ passive 
+    rotations, i.e. from sample to crystal coordinate system).
+
     """
 
     ax, ang = euler_pair_disorientation_all(eulers_A, eulers_B, point_group,
                                             degrees_in, degrees_out)
 
-    return (ax[0], ang)
+    return (ax[:, 0][:, np.newaxis], ang)
 
 
 def euler_pair_disorientation_all(eulers_A, eulers_B, point_group,
@@ -129,11 +131,6 @@ def euler_pair_disorientation_all(eulers_A, eulers_B, point_group,
 
     Symmetrically equivalent axes may be returned for one disorientation
     angle.
-
-    Notes
-    -----
-    Euler angles are expected in Bunge notation (intrinsic, ZXZ passive 
-    rotations, i.e. from sample to crystal coordinate system).
 
     Parameters
     ----------
@@ -159,6 +156,10 @@ def euler_pair_disorientation_all(eulers_A, eulers_B, point_group,
         angle : float
             Disorientation angle in radians or degrees.
 
+    Notes
+    -----
+    Euler angles are expected in Bunge notation (intrinsic, ZXZ passive 
+    rotations, i.e. from sample to crystal coordinate system).
 
     """
     sym_ops = SYM_OPS[point_group]
@@ -166,9 +167,9 @@ def euler_pair_disorientation_all(eulers_A, eulers_B, point_group,
     rot_A = rotations.euler2rot_mat_n(eulers_A, degrees=degrees_in)[0]
     rot_B = rotations.euler2rot_mat_n(eulers_B, degrees=degrees_in)[0]
 
-    mis_rot = np.ones((len(S)**2, 3, 3)) * np.nan
-    mis_ax = np.ones((len(S)**2, 3)) * np.nan
-    mis_ang = np.ones((len(S)**2)) * np.nan
+    mis_rot = np.ones((len(sym_ops)**2, 3, 3)) * np.nan
+    mis_ax = np.ones((len(sym_ops)**2, 3)) * np.nan
+    mis_ang = np.ones((len(sym_ops)**2)) * np.nan
 
     s_idx = 0
 
@@ -197,4 +198,78 @@ def euler_pair_disorientation_all(eulers_A, eulers_B, point_group,
     dis_ax = mis_ax[dis_idx]
     dis_ang = mis_ang[dis_idx]
 
-    return (dis_ax, dis_ang[0])
+    return (dis_ax.T, dis_ang[0])
+
+
+def gb_disorientation(eulers_A, eulers_B, point_group, lat, ang_tol,
+                      max_mill=20, degrees_in=False, degrees_out=False):
+    """
+    Find the disorientation axis and angle of a grain boundary from the Euler
+    angles of two grains.
+
+    Parameters
+    ----------
+    eulers_A : ndarray of shape (3,)
+        See notes.
+    eulers_B : ndarray of shape (3,)
+        See notes.
+    point_group : str
+        One of "6/mmm", "cubic", "triclinic"
+    lat : ndarray of shape (3, 3)
+        Column vectors representing the lattice unit cell 
+    ang_tol : float
+        Snapping angular tolerance of the disorientation axis.
+    max_mill: int, optional
+        Maximum Miller index. By default, set to 20.        
+    degrees_in : bool, optional
+        If True `eulers_A`, `eulers_B` and `ang_tol` values are interpreted as
+        degrees, otherwise as radians. False by default.
+    degrees_out : bool, optional
+        If True, disorientation angle and axis deviation from returned Miller
+        indices are returned in degrees, otherwise in radians. False by 
+        default.
+
+    Returns
+    -------
+    tuple of (ax_mill, ax_diff, ang)
+        ax_mill : ndarray of int shape (3, 1)
+            Integer column vector representing the disorientation axis in 
+            Miller indices.
+        ax_diff : float
+            Angular deviation of the found lattice disorientation axis from the
+            actual axis. In radians or degrees.
+        ang : float
+            Disorientation angle. In radians or degrees.
+
+    Notes
+    -----
+    Euler angles are expected in Bunge notation (intrinsic, ZXZ passive 
+    rotations, i.e. from sample to crystal coordinate system).
+
+    """
+
+    # Find the angle and disorientation axis in Cartesian coordinates
+    ax_cart, ang = euler_pair_disorientation_all(
+        eulers_A, eulers_B, point_group, degrees_in, degrees_out)
+
+    # print('ax_cart: \n{}\n'.format(ax_cart))
+
+    ax_mill_all = []
+    ax_diff_all = []
+
+    for ax in ax_cart.T:
+        # "Snap" the axis to the nearest lattice site within some angular tolerance
+        ax_mill, ax_diff = lattice.cart2miller_all(
+            ax[:, np.newaxis], lat, ang_tol, 'direction', max_mill, degrees_in, degrees_out)
+
+        ax_mill_all.append(ax_mill)
+        ax_diff_all.append(ax_diff)
+
+    ax_mill_all = np.hstack(ax_mill_all)
+    ax_diff_all = np.hstack(ax_diff_all)
+
+    sort_idx = np.argsort(ax_diff_all)
+    ax_mill_all = ax_mill_all[:, sort_idx]
+    ax_diff_all = ax_diff_all[sort_idx]
+
+    return (ax_mill_all[:, 0], ax_diff_all[0], ang)
