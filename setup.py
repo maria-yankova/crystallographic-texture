bbb1ebea6cb56d24a854fbc6dad4c4b0389afa84
maria-yankova <maria.yankova@manchester.ac.uk>
2021-05-03 21:24:09 +0100

    Prep for v0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='crystex',
-    version="0.1",
+    version="0.1.0",
     author='Maria S. Yankova, Adam J. Plowman',
     packages=find_packages(),
     install_requires=[
